602f71091c295dcd1e08beaa9dc5a9242bbaaadd
kaitabuchi <kaidanieltabuchi@gmail.com>
2022-01-07 12:11:03 -0600

    menu screen base
    menu screen base, welcome text, and nonworking new project button

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
-print("PyMakerStudio!")
+from tkinter import *
+
+App = Tk()
+
+App.title("PyMaker Studio")
+
+canvas = Canvas(App, width=1200, height=800)
+
+welcomeText = Label(App, text="Welcome to PyMaker Studio")
+newProjectButton = Button(App, text="New Project")
+
+
+welcomeText.pack()
+newProjectButton.pack()
+canvas.pack()
+App.mainloop()
