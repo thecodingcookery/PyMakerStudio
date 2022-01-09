07f7946f3b75f814847919aa4e76e73ae0bb3061
kaitabuchi <kaidanieltabuchi@gmail.com>
2022-01-08 19:42:23 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,11 +6,13 @@ App.title("PyMaker Studio")
 
 canvas = Canvas(App, width=1200, height=800)
 
-welcomeText = Label(App, text="Welcome to PyMaker Studio")
-newProjectButton = Button(App, text="New Project")
+welcomeText = Label(App, text="Welcome to PyMaker Studio", font="Helvetica 18 bold")
+space1 = Label(App)
+newProjectButton = Button(App, text="New Project", width=20)
 
 
 welcomeText.pack()
+space1.pack()
 newProjectButton.pack()
 canvas.pack()
 App.mainloop()
